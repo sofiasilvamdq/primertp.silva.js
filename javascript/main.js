9772952fd7227a05caf3eb3a0f61577a0c86738d
sofiasilvamdq <sofiasilva.mdq@gmail.com>
2023-01-03 15:26:22 -0300

    loarreglemasytengounaideamasparaponer

--- javascript/main.js
+++ javascript/main.js
@@ -1,78 +1,12 @@
-/*class Usuario {
-    constructor(nombre, alias, clave, saldo) {
+class Usuario {
+    constructor(nombre, apellido, alias, clave, ) {
         this.nombre = nombre;
+        this.apellido = apellido;
         this.alias = alias;
         this.clave = clave;
-        this.saldo = saldo;
     }
 }
 
-const cliente1 = new Usuario("Rodrigo", "Rodri90", "Chapa", 27000);
-const cliente2 = new Usuario("Sofia", "Sofi91", "Plato", 15000);
-const cliente3 = new Usuario("Benicio", "Beni25", "Roblox", 9000);
-
-const arrayUsuarios = [cliente1, cliente2, cliente3];
-
-
-console.log("Usuarios activos: ");
-console.log(arrayUsuarios);
-
-
-const usuario = arrayUsuarios.find(usuario => usuario.alias === "Rodri90");
-
-const usuarioAutorizado = "2";
-const claveAutorizada = "1234";
-const saldoInicial = 37000;
-
-arrayUsuarios.forEach((usuario) => { 
-    let usuarioIngresado = prompt("Ingrese su nombre de Usuario:");
-    if (usuarioIngresado === usuario.alias) {
-        for(let i = 0 ; i < 3 ; i++) {
-            let claveIngresada = prompt("Bienvenido/a " + usuario.nombre + ". Ingresa tu Clave:");
-            if (claveIngresada === usuario.clave) {
-                for(let i = 0 ; i < 1 ; i++) {
-                let saldoRetirado = parseInt(prompt("Saldo disponible: $" + usuario.saldo + ". Ingrese el monto a retirar: "))
-                let saldoDisponible = resta(usuario.saldo, saldoRetirado)
-                if (saldoRetirado <= usuario.saldo) {
-                    saldoRetirado = alert("Quedan $" + saldoDisponible + " en su cuenta")  
-                } else { saldoRetirado = alert("¡ERROR! Saldo insuficiente para realizar la operación")}
-            break;
-             };} else { claveIngresada = prompt("¡CONTRASEÑA INCORRECTA! Intente una vez más:")
-             if (claveIngresada === usuario.clave) {
-                for(let i = 0 ; i < 1 ; i++) {
-                let saldoRetirado = parseInt(prompt("Saldo disponible: $" + usuario.saldo + ". Ingrese el monto a retirar: "))
-                let saldoDisponible = resta(usuario.saldo, saldoRetirado)
-                if (saldoRetirado <= usuario.saldo) {
-                    saldoRetirado = alert("Quedan $" + saldoDisponible + " en su cuenta. Muchas gracias.")  
-                } else { saldoRetirado = alert("¡ERROR! Saldo insuficiente para realizar la operación")}
-            break;
-             };}
-            break;
-  
-  
-        }
-        
-        }
-
-    ;
-    } else {
-    usuarioIngresado = alert("¡Usuario incorrecto!");
-    }
-});
-
-
-function resta(valorA, valorB) {
-    let resultado = valorA - valorB;
-    return resultado;
-
- }*/
-
- function Usuario (nombre, apellido, alias, clave, ) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.alias = alias;
-    this.clave = clave;
-}
 
 
 const arrayUsuarioNuevo = [];
@@ -80,36 +14,35 @@ const arrayUsuarioNuevo = [];
 const formularioNuevoUsuario = document.getElementById("formularioNuevoUsuario");
 
 formularioNuevoUsuario.addEventListener("submit", (e)=>{
-e.preventDefault();
+    e.preventDefault();
 
-const nombre = document.getElementById("nombre").value;
-const apellido = document.getElementById("apellido").value;
-const alias = document.getElementById("alias").value;
-const clave = document.getElementById("clave").value;
-
-const usuario = new Usuario(nombre, apellido, alias, clave);
-arrayUsuarioNuevo.push(usuario);
+    const nombre = document.getElementById("nombre");
+    const apellido = document.getElementById("apellido");
+    const alias = document.getElementById("alias");
+    const clave = document.getElementById("clave");
+ 
+    const cliente = new Usuario(nombre.value, apellido.value, alias.value, clave.value);
+    arrayUsuarioNuevo.push(cliente);
 console.log(arrayUsuarioNuevo);
- formularioNuevoUsuario.reset();
-})
+     formularioNuevoUsuario.reset();
 
-const usuarioNuevoJSON = JSON.stringify(formularioNuevoUsuario);
-console.log(usuarioNuevoJSON, typeof usuarioNuevoJSON);
+const clienteJSON = JSON.stringify(arrayUsuarioNuevo);
+console.log(clienteJSON, typeof clienteJSON);
+
+localStorage.setItem("usuario", clienteJSON);
 
 
-localStorage.setItem("usuario", usuarioNuevoJSON);
 
-const usuarioRecuperado = localStorage.getItem("usario");
+const usuarioJSON = localStorage.getItem("usario");
 
-const usuarioObjeto = JSON.parse(usuarioRecuperado);
+const usuarioObjeto = JSON.parse(usuarioJSON);
 
 console.log(usuarioObjeto, typeof usuarioObjeto);
 
+const contenedorUsuarioNuevo = document.getElementById("formularioNuevoUsuario")
 
+usuarioObjeto.forEach(usuario => {
+    contenedorUsuarioNuevo.innerHTML += `<p> ${usuario.nombre} - ${usuario.alias} </p>`;
+});
 
-
-
-
-
-
-
+})
