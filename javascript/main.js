176f010d6a5280a1052d27e0a10ce6e29a7d5430
sofiasilvamdq <sofiasilva.mdq@gmail.com>
2023-01-27 22:18:36 -0300

    retoques

--- javascript/main.js
+++ javascript/main.js
@@ -1,57 +1,77 @@
 class Usuario {
-    constructor(nombre, apellido, username, password, saldo ) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.username = username;
-        this.password = password;
-        this.saldo = saldo;
-    }
+  constructor(nombre, apellido, username, password, saldo ) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.username = username;
+      this.password = password;
+      this.saldo = saldo;
+  }
 }
+const arrayUsuariosPorDefecto = []
+
+const usuarioPorDefecto1 = new Usuario("User1", "User1", "User1", "password", 0);
+const usuarioPorDefecto2 = new Usuario("User2", "User2", "User2", "password", 0);
+arrayUsuariosPorDefecto.push(usuarioPorDefecto1, usuarioPorDefecto2);
+const usuarioPorDefectoJSON = JSON.stringify(arrayUsuariosPorDefecto);
+console.log(usuarioPorDefectoJSON, typeof usuarioPorDefectoJSON);
+
+localStorage.setItem("usuario", usuarioPorDefectoJSON);
 
 
 const arrayUsuarioNuevo = [];
 
 const formularioNuevoUsuario = document.getElementById("formularioNuevoUsuario");
-
 formularioNuevoUsuario.addEventListener("submit", (e)=>{
-    e.preventDefault();
+  e.preventDefault();
 
-    const nombre = document.getElementById("nombre");
-    const apellido = document.getElementById("apellido");
-    const username = document.getElementById("username");
-    const password = document.getElementById("password");
-   
+  const nombre = document.getElementById("nombre");
+  const apellido = document.getElementById("apellido");
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
  
-    const cliente = new Usuario(nombre.value, apellido.value, username.value, password.value);
-    arrayUsuarioNuevo.push(cliente);
-    console.log(arrayUsuarioNuevo);
-    formularioNuevoUsuario.reset();
+
+  
+  const cliente = new Usuario(nombre.value, apellido.value, username.value, password.value);
+  
+  arrayUsuarioNuevo.push(cliente);
+  console.log(arrayUsuarioNuevo);
+  formularioNuevoUsuario.reset();
 
 const clienteJSON = JSON.stringify(arrayUsuarioNuevo);
 console.log(clienteJSON, typeof clienteJSON);
 
 localStorage.setItem("usuario", clienteJSON);
 
-const usuarioGuardadoJSON = localStorage.getItem("usuario");
 
-const usuarioRegistrado = JSON.parse(usuarioGuardadoJSON);
-
-console.log(usuarioRegistrado, typeof usuarioRegistrado);
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', enviar); 
-  });
+  document.getElementById("formulario").addEventListener('submit', enviar); 
+});
+
+
 
 function enviar(evento) {
-    evento.preventDefault();
-    var usuario = document.getElementById('usuario').value;
-    if(usuario = true) {
-  window.location.href="/index.html"
-    }
- 
-    this.submit();
-  }
+  evento.preventDefault();
+  return true;
+
+} if(enviar=true){
+
+  swal.fire({
+    icon:'ok',
+    title: "Usuario creado correctamente, será redirigido a la página principal",
+    background: "YellowGreen",
+    backdrop: "Teal",
+      
+  })
+  setTimeout(()=> {
+    window.location.href = "/paginas/ingreso.html";
+}, 5000)
+}
 
 
-})
+
+
+
+
+});
